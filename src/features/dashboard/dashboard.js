2947082ba060a46d31d9128ef92446c3017c6562
taimursaeed <taimur.saeed.malik@gmail.com>
2021-06-01 20:07:54 +0500

    dashboard layout

--- src/features/dashboard/dashboard.js
+++ src/features/dashboard/dashboard.js
@@ -1,12 +1,18 @@
+import { Box, Flex } from "@chakra-ui/react";
 import { BrowserRouter as Router } from "react-router-dom";
 import Main from "../main/main";
 import Sidebar from "../sidebar/sidebar";
-
 export default function Dashboard() {
   return (
     <Router>
-      <Sidebar />
-      <Main />
+      <Flex height="100%">
+        <Box bg="green.500" w="xs" p={8}>
+          <Sidebar />
+        </Box>
+        <Box bg="tomato" p={8} flex={1}>
+          <Main />
+        </Box>
+      </Flex>
     </Router>
   );
 }
